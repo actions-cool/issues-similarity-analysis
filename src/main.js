20cd7a9d2dbfaf2b39d8d9558b0ace443b116d25
xrkffgg <xrkffgg@vip.qq.com>
2021-03-21 16:56:28 +0800

    wip

--- src/main.js
+++ src/main.js
@@ -1,17 +1,62 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
+
+const { queryIssues, formatTitle, doIssueComment } = require('./public');
+
+const { dealStringToArr } = require('actions-util');
+
+const { compare } = require('compare-similarity');
+
+// ************************************************
+const context = github.context;
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const { owner, repo } = context.repo;
 
-    core.debug(new Date().toTimeString());
-    await new Promise(resolve => {
-      setTimeout(() => resolve('done!'), 10);
-    });
-    core.debug(new Date().toTimeString());
+    const FIXCOMMENT = `<!-- Created by actions-cool/issues-similarity-analysis. Do not remove. -->`;
 
-    core.setOutput('time', new Date().toTimeString());
+    if (context.eventName == 'issues') {
+      const { number, title } = context.payload.issue;
+
+      const sinceDays = core.getInput('since-days');
+      const since = dayjs.utc().subtract(sinceDays, 'day').format();
+
+      let issues = await queryIssues(owner, repo, since);
+
+      const filterThreshold = core.getInput('filter-threshold');
+      const titleExcludes = core.getInput('title-excludes');
+      const commentTitle = core.getInput('comment-title');
+      const commentBody = core.getInput('comment-body');
+
+      const result = [];
+      issues.forEach(issue => {
+        if (issue.pull_request === undefined) {
+          const formatIssT = formatTitle(dealStringToArr(titleExcludes), issue.title);
+          const formatT = formatTitle(dealStringToArr(titleExcludes), title);
+          const similarity = compare(formatIssT, formatT);
+          if (similarity >= filterThreshold) {
+            result.push({
+              number: issue.number,
+              title: issue.title,
+              similarity: similarity,
+            });
+          }
+        }
+      });
+
+      core.info(`[Action][filter-issues][length: ${result.length}]`);
+      if (result.length > 0) {
+        result.sort((a, b) => b.similarity - a.similarity);
+        await doIssueComment(owner, repo, number, result, commentTitle, commentBody, FIXCOMMENT);
+      }
+    } else {
+      core.setFailed(`This action only support on "issues"!`);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
---
+++ src/public.js
@@ -0,0 +1,101 @@
+const core = require('@actions/core');
+const { Octokit } = require('@octokit/rest');
+const token = core.getInput('token');
+const octokit = new Octokit({ auth: `token ${token}` });
+
+// ************************************************
+async function queryIssues(owner, repo, since, page = 1) {
+  let { data: issues } = await octokit.issues.listForRepo({
+    owner,
+    repo,
+    state: 'all',
+    since,
+    per_page: 100,
+    page,
+  });
+  if (issues.length >= 100) {
+    issues = issues.concat(await queryIssues(owner, repo, since, page + 1));
+  }
+  return issues;
+}
+
+function formatTitle(excludes, title) {
+  if (excludes.length == 0) {
+    return title;
+  }
+  excludes.forEach(ex => {
+    title = title.replace(ex, '');
+  });
+  return title.trim();
+}
+
+async function doIssueComment(owner, repo, number, issues, commentTitle, commentBody, FIXCOMMENT) {
+  const comments = await listComments(owner, repo, number);
+  const filterComments = [];
+  comments.forEach(comment => {
+    if (comment.body.includes(FIXCOMMENT)) {
+      filterComments.push(comment.id);
+    }
+  });
+  if (filterComments.length > 1) {
+    core.info(`Error: filterComments length is ${filterComments.length}.`);
+    return false;
+  }
+  const title = commentTitle || `Issues Similarity Analysis:`;
+  let body = '';
+  issues.forEach(iss => {
+    if (commentBody) {
+      let temp = commentBody;
+      temp = temp.replace('${number}', iss.number);
+      temp = temp.replace('${title}', iss.title);
+      temp = temp.replace('${similarity}', iss.similarity);
+      body += temp;
+    } else {
+      body += `- [#${iss.number}][${iss.title}][${iss.similarity}]`;
+    }
+  });
+
+  const footer = `<sub>🤖 By [issues-similarity-analysis](https://github.com/actions-cool/issues-similarity-analysis)</sub>
+
+${FIXCOMMENT}
+`;
+
+  if (filterComments.length == 0) {
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: number,
+      body: title + '\n' + body + '\n' + footer,
+    });
+    core.info(`Actions: [create-comment][${number}] success!`);
+  } else {
+    await octokit.issues.updateComment({
+      owner,
+      repo,
+      comment_id: filterComments[0],
+      body: title + '\n' + body + '\n' + footer,
+    });
+    core.info(`Actions: [update-comment][${number}] success!`);
+  }
+}
+
+async function listComments(owner, repo, number, page = 1) {
+  let { data: comments } = await octokit.issues.listComments({
+    owner,
+    repo,
+    issue_number: number,
+    per_page: 100,
+    page,
+  });
+  if (comments.length >= 100) {
+    comments = comments.concat(await listComments(owner, repo, number, page + 1));
+  }
+  return comments;
+}
+
+// ************************************************
+module.exports = {
+  queryIssues,
+  formatTitle,
+  doIssueComment,
+};
